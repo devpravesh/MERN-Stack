961331c03332b5473d0322f37714ed0c821d864e
Pravesh <pravesh2655@gmail.com>
2022-08-02 23:03:25 +0530

    kuch nahi

---
+++ src/component/users.jsx
@@ -0,0 +1,22 @@
+import logo from '../logo.svg';
+import '../App.css';
+
+function Users() {
+    <div className="App">
+        <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                USERS Page
+            </p>
+            <a
+                className="App-link"
+                href="/"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+        </header>
+    </div>
+}
+export default Users;
